b3f26a35cf405163e14e65a5ef4d771cb7e96b71
ThunderStones <879068849@qq.com>
2022-01-28 16:15:05 +0800

    init mypetstore-api

---
+++ src/main/java/com/org/mypetstore/api/MypetstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.org.mypetstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/mypetstore/api/MypetstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.mypetstore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
